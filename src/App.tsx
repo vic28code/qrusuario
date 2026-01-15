971e08a6eccb6fd426b637d17334c7c17844a4bc
Hilda Victoria Angulo Borja <134457751+vic28code@users.noreply.github.com>
2026-01-15 04:07:50 +0000

    path right

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+// 1. IMPORTANTE: Importar la página que creamos para el usuario
+import ConsultaTurno from "./pages/ConsultaTurno"; 
 
 const queryClient = new QueryClient();
 
@@ -16,6 +18,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          
+          {/* 2. IMPORTANTE: Agregar esta línea para que el link funcione */}
+          {/* El ":id" atrapará el número del turno (ej: A4485) */}
+          <Route path="/consulta-turno-:id" element={<ConsultaTurno />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
